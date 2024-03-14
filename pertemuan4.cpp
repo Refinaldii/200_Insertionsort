8091ed26708114ebd052652b2359e7f02dd49f94
Refinaldii <refinaldi9786@gmail.com>
2024-03-14 12:02:52 +0700

    membuat procedure input

--- pertemuan4.cpp
+++ pertemuan4.cpp
@@ -6,5 +6,18 @@ using namespace std;
 int arr[20];                                  //Membuat Array dengan panjang data 20
 int n;                                        //Membuat Variable Inputan n
 
+void input() {              //procedure input
+    while (true)
+    {
+        cout << "Masukan Jumlah Data pada Array : ";    //Membuat Imputan jumlah element array
+        cin >> n;                                       //Memanggil variable Inputan n
 
-
+        if (n <= 20) {                                  // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang ada masukan maksimal 20 Elemen. \n";  //menampilkan pesan
+        }
+    }
+   
