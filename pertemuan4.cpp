185969f26891018c47df2fb71134b075e83adb04
Refinaldii <refinaldi9786@gmail.com>
2024-03-14 11:57:09 +0700

    Add project files.

---
+++ pertemuan4.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+using namespace std;
+
+int arr[20];                                  //Membuat Array dengan panjang data 20
+int n;                                        //Membuat Variable Inputan n
+
+
+
